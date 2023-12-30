8a515534b95d95c73cee6b10cc4f760bf5d0afdf
Seung Hyeon Nam <namsh1125@naver.com>
2023-12-30 14:53:32 +0900

    [Init] 프로젝트 초기 설정

---
+++ src/main/java/kea/dpang/hrserver/HumanResourceTeamServerApplication.java
@@ -0,0 +1,13 @@
+package kea.dpang.hrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceTeamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanResourceTeamServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/dpang/hrserver/HumanResourceTeamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.dpang.hrserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceTeamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
