ea139991e5bedd139de50f4188f0ad679fdc6f96
SeungHyeon <namsh1125@naver.com>
2024-01-02 21:56:40 +0900

    [Refactor] DTO 사용해 리팩토링

--- src/main/java/kea/dpang/hrserver/controller/GlobalExceptionHandler.java
+++ src/main/java/kea/dpang/hrserver/controller/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package kea.dpang.hrserver.controller;
 
+import kea.dpang.hrserver.dto.ErrorResponseDto;
 import kea.dpang.hrserver.exception.EmailAlreadyExistsException;
 import kea.dpang.hrserver.exception.MemberNotFoundException;
 import lombok.extern.slf4j.Slf4j;
@@ -8,37 +9,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MemberNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleMemberNotFoundException(MemberNotFoundException ex) {
+    public ResponseEntity<ErrorResponseDto> handleMemberNotFoundException(MemberNotFoundException ex) {
         log.error("회원 조회 실패: 회원 ID - '{}'", ex.getMemberId());
 
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "존재하지 않는 회원입니다");
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDto("존재하지 않는 회원입니다."));
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<String> handleEmailAlreadyExistsException(EmailAlreadyExistsException e) {
+    public ResponseEntity<ErrorResponseDto> handleEmailAlreadyExistsException(EmailAlreadyExistsException e) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
+                .body(new ErrorResponseDto("이미 존재하는 이메일입니다."));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleGeneralException(Exception ex) {
+    public ResponseEntity<ErrorResponseDto> handleGeneralException(Exception ex) {
         log.error("Exception caught: ", ex);
 
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "예기치 않은 오류가 발생했습니다");
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponseDto("예기치 않은 오류가 발생했습니다."));
     }
 }
---
+++ src/main/java/kea/dpang/hrserver/dto/ErrorResponseDto.java
@@ -0,0 +1,8 @@
+package kea.dpang.hrserver.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ErrorResponseDto {
+    private String message;
+}
